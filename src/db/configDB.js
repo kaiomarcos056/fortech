a9d2c09788e3cef248e7daf358426262586c263f
Marcos Kaio <kaionery111@gmail.com>
2022-12-09 09:51:36 -0300

    Deploy

--- src/db/configDB.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3');
-const {open} = require('sqlite');
-
-exports.openDb = () => {
-	return open({
-		filename: './src/db/database.db',
-		driver: sqlite3.Database
-	})
-}
